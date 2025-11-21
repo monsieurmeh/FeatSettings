96ac5f9437ec206266283999c2832c7667372172
MonsieurMeh <monsieurmeh@gmail.com>
2025-11-21 10:14:04 -0800

    screw it, 1.2.0 - extra feats is totally a new feature

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("FeatSettings")]
 [assembly: AssemblyCopyright("MonsiuerMeh")]
-[assembly: AssemblyVersion("1.1.5")]
-[assembly: AssemblyFileVersion("1.1.5")]
-[assembly: MelonInfo(typeof(FeatSettings.Main), "FeatSettings", "1.1.5", "MonsieurMeh", null)]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: MelonInfo(typeof(FeatSettings.Main), "FeatSettings", "1.2.0", "MonsieurMeh", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
