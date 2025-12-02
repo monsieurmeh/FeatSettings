b5f3741d88f6a9b9289622413af384458ada7558
MonsieurMeh <monsieurmeh@gmail.com>
2025-12-01 23:24:36 -0800

    chore: 1.3.0 release

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("FeatSettings")]
 [assembly: AssemblyCopyright("MonsiuerMeh")]
-[assembly: AssemblyVersion("1.2.1")]
-[assembly: AssemblyFileVersion("1.2.1")]
-[assembly: MelonInfo(typeof(FeatSettings.Main), "FeatSettings", "1.2.1", "MonsieurMeh", null)]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: MelonInfo(typeof(FeatSettings.Main), "FeatSettings", "1.3.0", "MonsieurMeh", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
